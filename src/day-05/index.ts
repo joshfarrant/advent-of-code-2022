d00c1376e1a62d6981115293130e4ba638a49c5b
Josh Farrant <josh@farrant.me>
2022-12-07 15:09:26 +0000

    Hacky and imperative day 5

---
+++ src/day-05/index.ts
@@ -0,0 +1,105 @@
+import * as R from 'ramda';
+
+import { getInput } from '../utils';
+
+type Instruction = [number, number, number];
+
+const isInstruction = (
+  arr: unknown[],
+): arr is Instruction =>
+  arr.length === 3 &&
+  arr.every((x) => typeof x === 'number');
+
+const parseInstruction = R.pipe<
+  [string],
+  string[],
+  string[],
+  Instruction
+>(
+  R.match(/\D+(\d+)\D+(\d+)\D+(\d+)/),
+  R.takeLast(3),
+  // TODO Could I use an Option monad here?
+  R.map(Number),
+);
+
+const isLetter = (str: string): boolean =>
+  /[a-z]/i.test(str);
+
+const formatStart = R.pipe<
+  [string],
+  string[],
+  string[][],
+  string[][],
+  string[][]
+>(
+  R.split('\n'),
+  R.map(
+    R.pipe(
+      R.split(''),
+      R.splitEvery(4),
+      R.map(R.pipe(R.nth(1))),
+    ),
+  ),
+  R.transpose,
+  R.map(
+    R.pipe(
+      R.dropLast(1),
+      R.dropWhile((x) => x === ' '),
+    ),
+  ),
+);
+
+const formatInstructions = R.pipe(
+  R.split('\n'),
+  R.map(parseInstruction),
+);
+
+const formatInputData = R.pipe(
+  R.split('\n\n'),
+  ([start, instructions]) => ({
+    position: formatStart(start),
+    instructions: formatInstructions(instructions),
+  }),
+);
+
+const applyInstructions =
+  (moveAsGroup: boolean) =>
+  ({ position, instructions }) => {
+    let remainingInstructions = [...instructions];
+    let currentPosition = [...position];
+    while (remainingInstructions.length > 0) {
+      const [count, fromCol, toCol] =
+        remainingInstructions.shift();
+      const fromIdx = fromCol - 1;
+      const toIdx = toCol - 1;
+      const fromColumn = currentPosition[fromIdx];
+      const toColumn = currentPosition[toIdx];
+
+      const [itemsToMove, remainingItems] =
+        R.splitAt(count)(fromColumn);
+      currentPosition[fromIdx] = [...remainingItems];
+      const movedItems = moveAsGroup
+        ? itemsToMove
+        : itemsToMove.reverse();
+      currentPosition[toIdx] = [...movedItems, ...toColumn];
+    }
+
+    return R.pipe(
+      R.map(R.head),
+      R.join(''),
+    )(currentPosition);
+  };
+
+const challenge1 = R.pipe(
+  formatInputData,
+  applyInstructions(false),
+);
+const challenge2 = R.pipe(
+  formatInputData,
+  applyInstructions(true),
+);
+
+const inputData = getInput(import.meta.dir, 'input.txt');
+
+console.debug('Challenge 1: ', challenge1(inputData));
+console.debug('Challenge 2: ', challenge2(inputData));
